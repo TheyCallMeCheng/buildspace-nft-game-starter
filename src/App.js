c2439fdcbddbb819cca7d77d332d9df92cad12b6
TheyCallMeCheng <cheng-corp@protonmail.com>
2022-06-20 15:24:46 +0200

    Added connect wallet button

--- src/App.js
+++ src/App.js
@@ -7,6 +7,58 @@ const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+  const [currentAccount, setCurrentAccount] = React.useState(null);
+
+  const checkIfWalletIsConnected = async () => {
+    try{
+      const { ethereum } = window;
+
+      if(!ethereum) {
+        console.log("Make sure you have Metamask!");
+        return;
+      } else {
+
+        console.log("We have the ethereum object", ethereum);      
+        const accounts = await ethereum.request({ method: 'eth_accounts'});
+       
+        if(accounts.length !== 0) {
+          const account = accounts[0];
+          console.log("Found authorized account ", account);
+          setCurrentAccount(account);
+        }else {
+          console.log("No authorized account found");
+        }
+      }
+    }catch(e){
+      console.log(e);
+    }
+  };
+
+  const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
+
+      if(!ethereum){
+        alert("Get metamask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected ", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    }catch(e){
+      console.log(e);
+    }
+  }
+
+  React.useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
   return (
     <div className="App">
       <div className="container">
@@ -22,6 +74,12 @@ const App = () => {
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <button
+            className="cta-button connect-wallet-button"
+            onClick={connectWalletAction}
+          >
+            Connect wallet to get started
+          </button>
           <a
             className="footer-text"
             href={TWITTER_LINK}
